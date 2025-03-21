9d12d9f4dca8fc86eb7a95962d4868e598eb90c7
Andrey <andreyhitoshi1997@gmail.com>
2025-03-21 19:20:50 -0300

    Subindo

--- src/main/java/com/example/testspringpassword/infrastructure/LambdaHandler.java
+++ src/main/java/com/example/testspringpassword/infrastructure/LambdaHandler.java
@@ -10,9 +10,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 
-public class LambdaHandler implements RequestHandler<Map<String, Object>, CompletableFuture<Map<String, Object>>> {
+public class LambdaHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
     private final PasswordValidationService service;
     private final ObjectMapper objectMapper;
 
@@ -22,41 +21,39 @@ public class LambdaHandler implements RequestHandler<Map<String, Object>, Comple
     }
 
     @Override
-    public CompletableFuture<Map<String, Object>> handleRequest(Map<String, Object> input, Context context) {
-        return CompletableFuture.supplyAsync(() -> {
-            Map<String, Object> response = new HashMap<>();
-            try {
-                String body = (String) input.get("body");
-                if (body == null) {
-                    response.put("statusCode", 400);
-                    response.put("body", "{\"message\": \"Password does not meet requirements.\"}");
-                    return response;
-                }
-
-                JsonNode jsonNode = objectMapper.readTree(body);
-                String passwordValue = jsonNode.get("password").asText();
-                if (passwordValue == null) {
-                    response.put("statusCode", 400);
-                    response.put("body", "{\"message\": \"Password field is missing\"}");
-                    return response;
-                }
-
-                ValidationResponse validation = service.validate(passwordValue);
-                if (!validation.isValid()) {
-                    response.put("statusCode", 400);
-                    response.put("body", objectMapper.writeValueAsString(validation));
-                    return response;
-                }
-
-                response.put("statusCode", 200);
-                response.put("body", objectMapper.writeValueAsString(validation));
-            } catch (Exception e) {
-                context.getLogger().log("Error: " + e.getMessage());
-                response.put("statusCode", 500);
-                response.put("body", "{\"message\": \"Error: " + e.getMessage() + "\"}");
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            String body = (String) input.get("body");
+            if (body == null) {
+                response.put("statusCode", 400);
+                response.put("body", "{\"message\": \"Password does not meet requirements.\"}");
+                return response;
             }
-            response.put("headers", Map.of("Content-Type", "application/json"));
-            return response;
-        });
+
+            JsonNode jsonNode = objectMapper.readTree(body);
+            String passwordValue = jsonNode.get("password").asText();
+            if (passwordValue == null) {
+                response.put("statusCode", 400);
+                response.put("body", "{\"message\": \"Password field is missing\"}");
+                return response;
+            }
+
+            ValidationResponse validation = service.validate(passwordValue);
+            if (!validation.isValid()) {
+                response.put("statusCode", 400);
+                response.put("body", objectMapper.writeValueAsString(validation));
+                return response;
+            }
+
+            response.put("statusCode", 200);
+            response.put("body", objectMapper.writeValueAsString(validation));
+        } catch (Exception e) {
+            context.getLogger().log("Error: " + e.getMessage());
+            response.put("statusCode", 500);
+            response.put("body", "{\"message\": \"Error: " + e.getMessage() + "\"}");
+        }
+        response.put("headers", Map.of("Content-Type", "application/json"));
+        return response;
     }
 }
